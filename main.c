6c5cd10bda7ce7c027075460f9c41f975ceb5f4c
pognet <pogo@interbit.pl>
2018-10-12 15:40:37 +0200

    rok przestępny

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int rok;
+    bool przestepny;
+
+    printf("Podaj rok: ");
+    scanf("%i", &rok);
+
+    przestepny = ! (rok % 4) && rok % 100 || ! (rok % 400);
+
+    printf("%i", przestepny);
+}
